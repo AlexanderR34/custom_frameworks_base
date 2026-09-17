72ec88e291c7e9983bad4e6ea2314bbba379dcc2
AlexanderR34 <oharol888@gmail.com>
2026-09-17 04:12:59 +0000

    SystemUI: Fix RingtonePlayer R import for res package
    Change-Id: If7205e579379f9f5287d9664b40dd733f7798449

--- packages/SystemUI/src/com/android/systemui/media/RingtonePlayer.java
+++ packages/SystemUI/src/com/android/systemui/media/RingtonePlayer.java
@@ -43,7 +43,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.systemui.CoreStartable;
-import com.android.systemui.R;
+import com.android.systemui.res.R;
 import com.android.systemui.dagger.SysUISingleton;
 
 import java.io.IOException;
